6e283f4ffdd59bc821a9e6d16c927e52e984bf4a
Ivan Gomez <ivan.gquintero@alumnos.udg.mx>
2020-09-18 14:05:37 -0500

    Creacion del menu con switch

--- main.c
+++ main.c
@@ -2,10 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-
 int main(){
+    char op;
 
-printf("Hola\n");
+    do{
+        printf("1.- Capturar enteros\n");
+        printf("2.- Mostrar cadena n veces\n");
+        printf("3.- Personajes\n");
+        printf("0.- Salir\n");
+        scanf("%c",&op);
+        fflush(stdin);
 
+        switch(op){
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            default:
+                break;
+
+
+         }
+
+    }while (op != '0');
     return 0;
 }
