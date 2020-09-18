685af744a7ac8d168da8a1330efed1a9f6241bfb
Ivan Gomez <ivan.gquintero@alumnos.udg.mx>
2020-09-18 14:43:07 -0500

    Se creo la primera opcion para capturar 5 valores

--- main.c
+++ main.c
@@ -2,6 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
+
+void capturar_enteros();
+void capturar_enteros(){
+    int i,suma=0,enteros[5];
+    float promedio;
+    for(i=0; i<5; i++){
+        printf("Ingrese el valor del numero %d: ",i+1);
+        scanf("%d",&enteros[i]);
+        printf("\n");
+    }
+
+    for(i=0; i<5; i++){
+        printf("Valor del numero %d: %d\n",i+1,enteros[i]);
+    }
+    for(i=0; i<5; i++){
+        suma = suma + enteros[i];
+    }
+    printf("La suma de los valores es: %d\n",suma);
+    promedio = suma/5.0;
+    printf("El promedio de los valores es: %f\n",promedio);
+    system("PAUSE");
+    system("CLS");
+}
+
 int main(){
     char op;
 
@@ -12,9 +36,11 @@ int main(){
         printf("0.- Salir\n");
         scanf("%c",&op);
         fflush(stdin);
+         system("CLS");
 
         switch(op){
             case '1':
+                capturar_enteros();
                 break;
             case '2':
                 break;
